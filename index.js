c4e5b304326824649270f0cad3873e0b059870f7
hritick1 <80475837+hritick1@users.noreply.github.com>
2023-04-20 22:43:33 +0530

    Added codes for Authentication system using jwt

---
+++ index.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const app=express();
+const dotenv=require('dotenv');
+const mongoose=require('mongoose');
+const postRoute=require('./routes/posts');
+//creating env
+dotenv.config();
+//Import Routes
+const authRoute=require('./routes/auth');
+//connect to DB
+mongoose.connect(process.env.DB_CONNECT,{dbName:""});
+
+//Middleware
+app.use(express.json());
+
+
+app.use('/api/user',authRoute);
+app.use('/api/posts',postRoute);
+app.listen(3000,()=>console.log('Server is Running'));
