1dbace8665313176e1b4d34eb40e02f69763d01f
hritick1 <80475837+hritick1@users.noreply.github.com>
2023-04-24 00:08:58 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -54,8 +54,6 @@ router.post('/login',async(req,res)=>{
            const token=jwt.sign({_id: user._id},process.env.TOKEN_SECRET);
            res.header('auth-token',token).send(token);
 
-           res.send('Logged In!');
-
 });
 router.get("/",(req,res)=>{
       res.send("Welcome to Jwt Authentication System ,Use Postman to register or login");
